161b08176af0ea0d763f324fc4b11ef30b1b28a3
David Jones <djones6@uk.ibm.com>
2019-01-30 14:06:14 +0000

    Pure swift implementation of removePercentEncoding

--- Sources/Kitura/String+Extensions.swift
+++ Sources/Kitura/String+Extensions.swift
@@ -77,12 +77,13 @@ extension Substring {
         value.replaceCharacters("+", with: " ")
 
 // TEMPORARY - evaluate benefit of removing this NSString method
-return (key: key, value: value)
-        let decodedValue = value.removingPercentEncoding
-        if decodedValue == nil {
-            Log.warning("Unable to decode query parameter \(key) (coded value: \(value)")
-        }
-        return (key: key, value: decodedValue != nil ? Substring(decodedValue!) : value)
+        value.removePercentEncoding()
+        return (key: key, value: value)
+//        let decodedValue = value.removingPercentEncoding
+//        if decodedValue == nil {
+//            Log.warning("Unable to decode query parameter \(key) (coded value: \(value)")
+//        }
+//        return (key: key, value: decodedValue != nil ? Substring(decodedValue!) : value)
     }
 
     /// Finds and replaces all occurrences of a character with the provided substring
@@ -95,5 +96,59 @@ return (key: key, value: value)
             }
             self.replaceSubrange(startIndex...startIndex, with: dst)
         } while true
+    }
+
+    /// Lookup table of valid hex characters
+    private static let validHexChars: [Character: UInt8] = [
+        "0": 0x00, "1": 0x01, "2": 0x02, "3": 0x03, "4": 0x04, "5": 0x05, "6": 0x06,
+        "7": 0x07, "8": 0x08, "9": 0x09, "a": 0x0A, "A": 0x0A, "b": 0x0B, "B": 0x0B,
+        "c": 0x0C, "C": 0x0C, "d": 0x0D, "D": 0x0D, "e": 0x0E, "E": 0x0E, "f": 0x0F,
+        "F": 0x0F
+    ]
+
+    /// Process this substring, replacing each valid percent-escaped sequence with
+    /// the corresponding character.
+    /// This implementation differs subtly from the CoreFoundation approach, in that
+    /// we will replace all valid percent-escaped sequences whilst ignoring any
+    /// invalid ones.
+    private mutating func removePercentEncoding() {
+        guard self.count > 2 else {
+            // Failure - string is too short to contain a valid escape sequence
+            return
+        }
+        var currentIndex = self.startIndex
+        repeat {
+            let char = self[currentIndex]
+            switch char {
+            case "%":
+                let hexChar1 = self.index(after: currentIndex)
+                guard hexChar1 < self.endIndex else {
+                    // Failure - invalid escape sequence (EOF)
+                    return
+                }
+                let hexChar2 = self.index(after: hexChar1)
+                guard hexChar2 < self.endIndex else {
+                    // Failure - invalid escape sequence (EOF)
+                    return
+                }
+                // get the hex digits
+                let hex1check = Substring.validHexChars[self[hexChar1]];
+                let hex2check = Substring.validHexChars[self[hexChar2]];
+                guard let hex1 = hex1check, let hex2 = hex2check else {
+                    // Failure - invalid hex sequence - but we can try to continue
+                    break
+                }
+                // convert hex digits
+                let resultingByte = (hex1 << 4) + hex2
+                let resultingChar = UnicodeScalar(resultingByte)
+                let resultingCharSequence = [Character(resultingChar)]
+                // assign result
+                self.replaceSubrange(currentIndex...hexChar2, with: resultingCharSequence)
+            default:
+                // Not the start of an escape sequence, carry on
+                break
+            }
+            currentIndex = self.index(after: currentIndex)
+        } while currentIndex < self.endIndex
     }
 }
